859f1fe085b6263ad259d519466e657831d82915
Ahaider60 <alihaiderizvi.you@gmail.com>
2021-07-22 07:32:48 +0500

    Updates

---
+++ precache-manifest.9718c1321c14aaed2ae589857db3b75d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "129fab9df562d0432811",
+    "url": "/Weather-App/static/css/main.482546a2.chunk.css"
+  },
+  {
+    "revision": "129fab9df562d0432811",
+    "url": "/Weather-App/static/js/main.e14e3471.chunk.js"
+  },
+  {
+    "revision": "89bb695666ed5c38f231",
+    "url": "/Weather-App/static/js/runtime~main.671df5a1.js"
+  },
+  {
+    "revision": "58ea823afec174179839",
+    "url": "/Weather-App/static/js/2.a01c4f0b.chunk.js"
+  },
+  {
+    "revision": "12352a50961ef6870bdc7fa832593415",
+    "url": "/Weather-App/index.html"
+  }
+];
